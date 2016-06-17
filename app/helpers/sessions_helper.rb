e90fe35935eb41fa6d7a6b62a1b6c78b01ca2354
Charles Mednieks <cmednieks@umass.edu>
2016-06-17 10:19:08 -0400

    added admin log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,12 +6,18 @@ module SessionsHelper
   
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= AdminUser.find_by(id: session[:user_id])
+    @current_user ||= AdminUser.find_by(id: session[:admin_user_id])
   end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+  
+  # Logs out the current user.
+  def log_out
+    session.delete(:admin_user_id)
+    @current_user = nil
   end
   
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,4 +2,11 @@ class AdminUser < ActiveRecord::Base
   validates :username, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  # Returns the hash digest of the given string.
+  def AdminUser.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
